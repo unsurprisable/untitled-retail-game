738d543f2e2565b01590b863c552520cdc08569d
Surprisable <toastedflqme@gmail.com>
2024-09-10 16:45:39 -0700

    extremely basic pause menu

--- Untitled Retail Game/Assets/Scripts/UI/PauseMenuUI.cs
+++ Untitled Retail Game/Assets/Scripts/UI/PauseMenuUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuUI : Menu
 {
@@ -9,5 +10,22 @@ public class PauseMenuUI : Menu
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void ResumeGame()
+    {
+        Hide();
+    }
+    public void OpenSettings()
+    {
+        Debug.Log("settings are not a thing yet");
+    }
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
