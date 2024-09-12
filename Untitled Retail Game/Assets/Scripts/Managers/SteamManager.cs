9c8e02c16cf351be38a760a5a92e73357e410e16
ImSurprisable <toastedflqme@gmail.com>
2024-09-11 17:49:00 -0700

    Revert "trying chat message instead"
    This reverts commit 481961251b2620b6a47c5c2265d07c19143b93b9.

--- Untitled Retail Game/Assets/Scripts/Managers/SteamManager.cs
+++ Untitled Retail Game/Assets/Scripts/Managers/SteamManager.cs
@@ -23,20 +23,15 @@ public class SteamManager : MonoBehaviour
     {
         SteamMatchmaking.OnLobbyCreated += SteamMatchmaking_OnLobbyCreated;
         SteamMatchmaking.OnLobbyEntered += SteamMatchmaking_OnLobbyEntered;
-        SteamMatchmaking.OnChatMessage += SteamMatchmaking_OnChatMessage;
+        SteamMatchmaking.OnLobbyMemberJoined += SteamMatchmaking_OnLobbyMemberJoined;
         SteamFriends.OnGameLobbyJoinRequested += SteamFriends_OnGameLobbyJoinRequested;
-    }
-
-    private void SteamMatchmaking_OnChatMessage(Lobby lobby, Friend friend, string message)
-    {
-        Debug.Log("Chat message: " + message);
     }
 
     private void OnDisable()
     {
         SteamMatchmaking.OnLobbyCreated -= SteamMatchmaking_OnLobbyCreated;
         SteamMatchmaking.OnLobbyEntered -= SteamMatchmaking_OnLobbyEntered;
-        SteamMatchmaking.OnChatMessage -= SteamMatchmaking_OnChatMessage;
+        SteamMatchmaking.OnLobbyMemberJoined -= SteamMatchmaking_OnLobbyMemberJoined;
         SteamFriends.OnGameLobbyJoinRequested -= SteamFriends_OnGameLobbyJoinRequested;
     }
 
@@ -62,8 +57,12 @@ public class SteamManager : MonoBehaviour
     {
         GameLobby.Instance.CurrentLobby = lobby;
         Debug.Log("entered a Lobby with ID: " + lobby.Id);
-        lobby.SendChatString(SteamClient.Name + " joined the lobby!");
         OnGameLobbyEntered?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void SteamMatchmaking_OnLobbyMemberJoined(Lobby lobby, Friend friend)
+    {
+        Debug.Log("wow! another user named + " + friend.Name + "joined your lobby!");
     }
 
     private void SteamFriends_OnGameLobbyJoinRequested(Lobby lobby, SteamId id)
